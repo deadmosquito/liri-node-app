b0abf2c8023adbba9f30dc8fbf4b630a5bd0a33f
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-08-23 12:40:02 -0700

    established variables

--- liri.js
+++ liri.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var fs = require("fs"); 
+var path = require("path"); 
+var axios = require("axios");
+var moment = require("moment");
+var action = process.argv[2];
+var parameter = process.argv.splice(3).join(" ");

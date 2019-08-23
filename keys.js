f314dad6f89a0028312df596cbddd4a974317031
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-08-23 12:33:11 -0700

    updated keys

--- keys.js
+++ keys.js
@@ -4,3 +4,11 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.omdb = {
+  key: process.env.OMDB_API_KEY,
+};
+
+exports.bandsInTown = {
+  id: process.env.BANDSINTOWN_CLIENT_ID,
+};
